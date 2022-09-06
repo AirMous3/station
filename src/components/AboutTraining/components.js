794d07dbfea97764fa3256616c4accfb0df81405
AirMouse <airmouselol@gmail.com>
2022-09-06 23:53:46 +0300

    improve: add hover background effect

--- src/components/AboutTraining/components.js
+++ src/components/AboutTraining/components.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 import cross from '@/assets/icons/cross.svg';
@@ -26,18 +27,45 @@ export const FeaturesWrapper = styled.ul`
   margin-bottom: 96px;
 `;
 
-export const Feature = styled.li`
+export const Cross = styled.div`
+  position: relative;
+  margin-left: auto;
+  background-image: url(${cross});
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 15px;
+  height: 15px;
+  margin-right: 15px;
+  cursor: pointer;
+
+  &:active {
+    transform: rotate(47.15deg);
+    transition: all 0.1s ease-out;
+  }
+`;
+
+export const Feature = styled(motion.li)`
+  position: relative;
   display: flex;
   align-items: center;
   // TODO
   border-bottom: 1px solid #535353;
+  color: ${({ theme }) => theme.colors.text.main};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.text.darkGrey};
+
+    ${Cross} {
+      filter: brightness(0);
+    }
+  }
 `;
 
 export const FeaturesTitle = styled.div`
   position: relative;
   font-weight: 700;
   font-size: ${({ theme }) => theme.typography.size[4]};
-  color: ${({ theme }) => theme.colors.text.main};
+  color: inherit;
   padding-bottom: 32px;
   padding-top: 42px;
 
@@ -57,18 +85,12 @@ export const FeaturesTitle = styled.div`
   }
 `;
 
-export const Cross = styled.div`
-  margin-left: auto;
-  background-image: url(${cross});
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 15px;
-  height: 15px;
-  margin-right: 15px;
-  cursor: pointer;
-
-  &:active {
-    transform: rotate(47.15deg);
-    transition: all 0.1s ease-out;
-  }
+export const Background = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  bottom: 1px;
+  left: 0;
+  background-color: ${({ theme }) => theme.colors.text.main};
+  z-index: -1;
 `;
--- src/components/AboutTraining/index.jsx
+++ src/components/AboutTraining/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { trainingFeaturesConfig } from '@/components/AboutTraining/config';
 import { Button } from '@/components/Button';
 import { Container } from '@/components/Container';
@@ -5,6 +7,8 @@ import { Container } from '@/components/Container';
 import * as S from './components';
 
 export const AboutTraining = () => {
+  const [hoveredFeature, setHoveredFeature] = useState();
+
   return (
     <Container>
       <S.Section>
@@ -17,12 +21,20 @@ export const AboutTraining = () => {
 
         <S.FeaturesWrapper>
           {trainingFeaturesConfig.map(({ number, title }) => (
-            <S.Feature key={number}>
+            <S.Feature
+              key={number}
+              onPointerOver={() => setHoveredFeature(number)}
+              onPointerOut={() => setHoveredFeature(undefined)}
+            >
+              {hoveredFeature === number && <S.Background layoutId="background" />}
+
               <S.FeaturesTitle number={number}>{title}</S.FeaturesTitle>
+
               <S.Cross />
             </S.Feature>
           ))}
         </S.FeaturesWrapper>
+
         <Button>Записаться</Button>
       </S.Section>
     </Container>
