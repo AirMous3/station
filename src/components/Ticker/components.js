26c65254e88edfc311dbd629840ae50e2035b334
AirMouse <airmouselol@gmail.com>
2022-11-04 22:46:56 +0300

    fix: css

--- src/components/Ticker/components.js
+++ src/components/Ticker/components.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  font-family: Generator;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.text.main};
   font-size: ${({ theme }) => theme.typography.size[13]};
   text-transform: uppercase;
   width: 100%;
   white-space: nowrap;
+  letter-spacing: 0.07em;
 `;
